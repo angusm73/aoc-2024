382ed94235d3b099a64fb4864b9b1118e3aa0961
Angus Moore <angusmoore73@gmail.com>
2024-12-02 18:06:02 +0800

    Refactor day 2 pt 1

--- 2 - Red-Nosed Reports/code_part_1.rb
+++ 2 - Red-Nosed Reports/code_part_1.rb
@@ -1,41 +1,22 @@
 reports = File.readlines('./2 - Red-Nosed Reports/input')
 
-class UnsafeLevels < StandardError; end
-
-def ascending?(levels)
-  levels.first < levels.last
-end
-
-def safe_step?(level, next_level, direction)
-  difference = next_level - level
-
-  safe_range = if direction == :asc
-                 (1..3)
-               else
-                 (-3..-1)
-               end
-
-  safe_range.include?(difference)
-end
-
 def safe?(levels)
-  direction = ascending?(levels) ? :asc : :desc
-  levels.each_with_index do |level, index|
-    next if index.zero? || safe_step?(levels[index - 1], level, direction)
-
-    raise UnsafeLevels
+  # Check if all increasing
+  increasing = levels.each_cons(2).all? do |a, b|
+    (b - a).between?(1, 3)
   end
 
-  true
-rescue UnsafeLevels
-  false
+  # Check if all decreasing
+  decreasing = levels.each_cons(2).all? do |a, b|
+    (a - b).between?(1, 3)
+  end
+
+  increasing || decreasing
 end
 
-total_safe = 0
-
-reports.each do |report|
+total_safe = reports.count do |report|
   levels = report.split.map(&:to_i)
-  total_safe += 1 if safe?(levels)
+  safe?(levels)
 end
 
 puts total_safe
