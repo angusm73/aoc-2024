37454c38fd309972788701262eea2387cf8e0a6f
Angus Moore <angusmoore73@gmail.com>
2024-12-03 13:50:10 +0800

    Refactor day 3

--- 2024/3 - Mull It Over/part1.rb
+++ 2024/3 - Mull It Over/part1.rb
@@ -2,10 +2,8 @@ input = File.read(File.join(File.dirname(__FILE__), './input'))
 
 VALID_INSTRUCT_REGEX = /mul\(([0-9]{1,3}),([0-9]{1,3})\)/.freeze
 
-sum = 0
-
-input.scan(VALID_INSTRUCT_REGEX).each do |num1, num2|
-  sum += num1.to_i * num2.to_i
-end
+sum = input.scan(VALID_INSTRUCT_REGEX)
+           .map { |num1, num2| num1.to_i * num2.to_i }
+           .sum
 
 puts sum
--- 2024/3 - Mull It Over/part2.rb
+++ 2024/3 - Mull It Over/part2.rb
@@ -3,12 +3,10 @@ input = File.read(File.join(File.dirname(__FILE__), './input'))
 INVALID_SECTION_REGEX = /don't\(\).+?(do\(\)|$)/.freeze
 VALID_INSTRUCT_REGEX  = /mul\(([0-9]{1,3}),([0-9]{1,3})\)/.freeze
 
-sum = 0
+valid_memory = input.gsub(/\n+\s*/m, ' ').gsub(INVALID_SECTION_REGEX, '')
 
-valid_input = input.gsub(/\n+\s*/m, ' ').gsub(INVALID_SECTION_REGEX, '')
-
-valid_input.scan(VALID_INSTRUCT_REGEX).each do |num1, num2|
-  sum += num1.to_i * num2.to_i
-end
+sum = valid_memory.scan(VALID_INSTRUCT_REGEX)
+                  .map { |num1, num2| num1.to_i * num2.to_i }
+                  .sum
 
 puts sum
