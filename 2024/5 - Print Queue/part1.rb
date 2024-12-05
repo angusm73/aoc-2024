3e4292527c9aeb8c3dde70e31423dfc4600fb329
Angus Moore <angusmoore73@gmail.com>
2024-12-05 17:57:36 +0800

    Day 5 - Print Queue (pt1)

---
+++ 2024/5 - Print Queue/part1.rb
@@ -0,0 +1,36 @@
+input = File.readlines(File.join(File.dirname(__FILE__), './input'))
+
+rules   = []
+updates = []
+
+input.each do |line|
+  rules << line.chomp.split('|').map(&:to_i) if line.include?('|')
+  updates << line.chomp.split(',').map(&:to_i) if line.include?(',')
+end
+
+def build_ordering_graph(rules)
+  graph = Hash.new { |h, k| h[k] = Set.new }
+
+  rules.each { |(before, after)| graph[before].add(after) }
+
+  graph
+end
+
+def allowed?(pages, ordering_graph)
+  pages.each_with_index do |page_num, index|
+    pages[..index].each do |prev_page|
+      if ordering_graph[page_num].include?(prev_page) &&
+         pages.index(page_num) > pages.index(prev_page)
+        return false
+      end
+    end
+
+    true
+  end
+end
+
+ordering_graph  = build_ordering_graph(rules)
+allowed_updates = updates.filter { |update| allowed?(update, ordering_graph) }
+middle_page_sum = allowed_updates.map { |update| update[update.length / 2] }.sum
+
+puts middle_page_sum
